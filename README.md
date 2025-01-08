da8aec5897bd541c0fc1516751fe00e6196a368b
Akshay More <45824962+theakshaymore@users.noreply.github.com>
2025-01-08 19:18:36 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Desktop_Assistant
+# How to Run 
+1. Create a new vartual environment
+
+```bash 
+conda create -n assistant python=3.7
+```
+
+
+2. checkout the created vartual environment
+
+```bash
+conda env list
+```
+
+3. Activate the vartual environment
+```bash
+conda active assistant
+```
+
+4. Install all the packages present in the requrment file
+```bash
+pip install -r Requirement.txt
+```
+
+
+# Requried GitHub commands
+```bash
+git add .
+
+git commit -m "massage"
+
+git push origin main
+```
+# Add me
---
+++ main.py
@@ -0,0 +1,46 @@
+import pyttsx3, speech_recognition as sr, datetime, wikipedia as wp, webbrowser, os
+             ####### TEXT---TO---SPEECH #######
+# Taking voice from my system
+engine = pyttsx3.init('sapi5') 
+voices = engine.getProperty('voices') #Note= getProperty('voices')= mean we get the (voices) property from pyttsx3.init.    
+#print(voices[1].id)
+
+engine.setProperty('voice', voices[1].id) # all set, we get voices-property from pyttsx3 and we use (setProperty)
+engine.setProperty('rate',120)
+
+# speak voice function
+
+## when we get 2 thing for voice function then [which 2 function {getProperty},{setProperty}]
+def speak(text):
+    '''This function takes text and returns a voice
+    '''
+    engine.say(text)
+    engine.runAndWait()
+
+#speak('hello Love, How are you? I am in love with you sweety ')
+
+               #####SPEECH---TO---TEXT#######
+
+#speech-recognition-function
+
+def takeCommand():
+    '''This function takes voice from user and returns it as text
+    '''
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Listening...')
+        r.pause_threshold = 2
+        audio = r.listen(source)
+        
+        try:
+            print('Recognizing...')
+            query = r.recognize_google(audio, language='en-in')
+            print(f"User said: {query}\n")
+        except Exception as e:
+            print("Say that again please...")
+            return "none"
+        return query
+
+text= takeCommand()        
+speak(text)
+
